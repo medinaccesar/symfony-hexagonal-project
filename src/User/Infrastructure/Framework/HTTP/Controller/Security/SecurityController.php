82665bc8c907788647dd78325e8f83d5d5336b64
Antonio Carreras <antonio.carreras@ui1.es>
2023-10-25 12:26:37 +0200

    update User.orm.xml to annotation

--- src/User/Infrastructure/Framework/HTTP/Controller/Security/SecurityController.php
+++ src/User/Infrastructure/Framework/HTTP/Controller/Security/SecurityController.php
@@ -7,12 +7,12 @@ namespace User\Infrastructure\Framework\HTTP\Controller\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use User\Infrastructure\Persistence\Mapping\User;
+use User\Infrastructure\Persistence\Mapping\DoctrineUser;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'login', methods: ['POST'])]
-    public function __invoke(User $user): Response
+    public function __invoke(DoctrineUser $user): Response
     {
         return $this->json($user);
     }
--- tests/User/Application/UseCase/GetUserByIdTest.php
+++ tests/User/Application/UseCase/GetUserByIdTest.php
@@ -2,7 +2,7 @@
 
 namespace App\Tests\User\Application\UseCase;
 
-use App\User\Application\UseCase\GetUserById\GetUserById;
+use User\Application\UseCase\GetUserById\GetUserByIdUseCase;
 use PHPUnit\Framework\TestCase;
 use User\Domain\Model\User;
 use User\Domain\Repository\UserRepositoryInterface;
@@ -12,11 +12,11 @@ class GetUserByIdTest extends TestCase
     public function testGetUserById(): void
     {
         $userRepository = $this->createMock(UserRepositoryInterface::class);
-        $getUserById = new GetUserById($userRepository);
+        $getUserById = new GetUserByIdUseCase($userRepository);
         $user = new User(1, 'example_username', 'example_password');
 
         $userRepository->method('findById')->willReturn($user);
-        $resultUser = $getUserById->execute(1);
+        $resultUser = $getUserById->handle(1);
 
         $this->assertInstanceOf(User::class, $resultUser);
         $this->assertEquals(1, $resultUser->getId());
