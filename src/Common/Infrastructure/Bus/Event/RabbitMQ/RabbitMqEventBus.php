41c47c5b8d0e8703add7bbc3f7bc3710db2421ef
Antonio Carreras <antoniocarrerasalarcon@gmail.com>
2023-11-21 18:04:10 +0100

    resolve rabbitmq

--- src/Common/Infrastructure/Bus/Event/RabbitMQ/RabbitMqEventBus.php
+++ src/Common/Infrastructure/Bus/Event/RabbitMQ/RabbitMqEventBus.php
@@ -7,6 +7,9 @@ namespace Common\Infrastructure\Bus\Event\RabbitMQ;
 use AMQPException;
 use Common\Domain\Bus\Event\DomainEvent;
 use Common\Domain\Bus\Event\EventBusInterface;
+use Common\Infrastructure\Bus\Event\DomainEventJsonSerializer;
+use Common\Infrastructure\Bus\Event\MySql\MySqlDoctrineEventBus;
+use Exception;
 
 /**
  * RabbitMQ implementation of an event bus.
@@ -38,6 +41,7 @@ final class RabbitMqEventBus implements EventBusInterface
      * Publishes domain events to the RabbitMQ exchange.
      *
      * @param DomainEvent ...$events Domain events to publish.
+     * @throws Exception
      */
     public function publish(DomainEvent ...$events): void
     {
@@ -50,12 +54,26 @@ final class RabbitMqEventBus implements EventBusInterface
      * Publishes a single domain event, with failover handling.
      *
      * @param DomainEvent $event The domain event to publish.
+     * @throws Exception
      */
     private function publishEvent(DomainEvent $event): void
     {
         try {
-            // Publish logic here, similar to your original implementation.
-        } catch (AMQPException $exception) {
+            $body = DomainEventJsonSerializer::serialize($event);
+            $routingKey = $event::eventName();
+            $messageId = $event->eventId();
+
+            $this->connection->exchange($this->exchangeName)->publish(
+                $body,
+                $routingKey,
+                AMQP_NOPARAM,
+                [
+                    'message_id' => $messageId,
+                    'content_type' => 'application/json',
+                    'content_encoding' => 'utf-8',
+                ]
+            );
+        } catch (AMQPException) {
             $this->failoverPublisher->publish($event);
         }
     }
